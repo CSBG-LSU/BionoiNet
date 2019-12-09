fad4a7ba0ee451454b239842fa9304021c25ae01
Wentao <wentao771@gmail.com>
2019-12-09 16:08:38 -0600

    updated for hyper-parameter tunining and the best model is now model with minimum loss instead of mcc

---
+++ bionoi_cnn_homology_reduced/resnet18.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#PBS -q k40
+#PBS -l nodes=1:ppn=20
+#PBS -l walltime=72:00:00
+#PBS -N bionoi_resnet18
+#PBS -A loni_omics01
+#PBS -j oe
+
+module purge
+source activate pytorch
+cd /work/wshi6/deeplearning-data/BionoiNet-prj/BionoiNet/bionoi_cnn_homology_reduced/
+python homology_reduced_cnn_cv_resnet18.py -op control_vs_nucleotide > ./log/control_vs_nucleotide_res18_second_run.log 2>&1
+
+
+
+
+
